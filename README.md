702007a6b31665f55b1bb0d81402b7c5a6331970
Vinicius Ribeiro <viniciusribeiro113@gmail.com>
2021-09-02 13:35:29 -0300

    Primeira etapa do projeto concluida
    Ainda com um bug, na proxima etapa adicionarei um laço para analise da existencia do arquivo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # zipFile
-Programa pra descompactar arquivos zipados
+## Programa pra descompactar arquivos zipados
+
+
+Precisei de uma forma de descompactar muitos arquivos zipado para um cliente, então fiz um código utilizando a classe nativa do C# ZipFile, para funcionar e preciso que façamos o seguinte:
+
+Criar uma pasta chamada zip no disco local C:, ficando assim:
+C:\zip
+
+Dentro da pasta zip, criar duas pastas: 
+Compactar e Descompactar.
+Uma pasta ficará os arquivos a serem executados, a outra os arquivos já descompactados.
+O resultado será esse:
+
+C:\zip\Compactar
+C:\zip\Descompactado
+
+Ao clicar em Compactar, o arquivo zipado será encontrado em:
+C:\zip\Resultado.zip
+
---
+++ ZipApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZipApp
+{
+    public partial class MainWindow : Window
+    {
+        string startPath = @"c:\zip\Compactar";
+        string zipPath = @"c:\zip\resultado.zip";
+        string extractPath = @"c:\zip\Descompactado";
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
